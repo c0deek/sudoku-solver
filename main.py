c74588965cfb81c6f41db13026741604158354b3
Prateek Tripathi <ptripathi@mt.iitr.ac.in>
2019-12-22 17:07:51 +0530

    new file

---
+++ main.py
@@ -0,0 +1,148 @@
+import os, time
+
+problem = [
+	[0, 0, 6, 0, 3, 0, 2, 0, 8,], 
+	[7, 0, 0, 0, 2, 0, 0, 0, 4,],
+	[0, 9, 2, 0, 0, 4, 1, 0, 0,],
+	[1, 0, 0, 2, 0, 0, 0, 0, 6,],
+	[0, 0, 8, 0, 0, 5, 0, 1, 0,],
+	[0, 2, 0, 0, 1, 0, 0, 4, 0,],
+	[8, 0, 0, 4, 0, 0, 0, 0, 0,],
+	[0, 0, 5, 0, 6, 0, 4, 8, 0,],
+	[0, 1, 0, 0, 0, 2, 0, 0, 3,],
+]
+
+
+filled = [
+	[0, 0, 0, 0, 0, 0, 0, 0, 0,],
+	[0, 0, 0, 0, 0, 0, 0, 0, 0,],
+	[0, 0, 0, 0, 0, 0, 0, 0, 0,],
+	[0, 0, 0, 0, 0, 0, 0, 0, 0,],
+	[0, 0, 0, 0, 0, 0, 0, 0, 0,],
+	[0, 0, 0, 0, 0, 0, 0, 0, 0,],
+	[0, 0, 0, 0, 0, 0, 0, 0, 0,],
+	[0, 0, 0, 0, 0, 0, 0, 0, 0,],
+	[0, 0, 0, 0, 0, 0, 0, 0, 0,],
+]
+
+
+def getProblem(problem):
+	print("Enter the board values(enter 0 if blank): ")
+	for i in range(0,9):
+		for j in range(0,9):
+			x = int(input("Enter row-wise nd hit enter after eachh value:\n"))
+			board[i][j] = x
+
+
+def showBoard(board):
+	for i in range(0,9):
+		print('')
+		if i==3 or i==6:
+			print("---------------------")
+		for j in range(0,9):
+			if j==3 or j==6:
+				print("|", end = ' ')
+			if(not board[i][j]):
+				print(' ', end=' ')
+			else:
+				print(board[i][j], end=' ')
+	print('\n')
+
+
+def createSuppliment(board, inverse):
+	for i in range(0,9):
+		for j in range(0,9):
+			if(not board[i][j]):
+				inverse[i][j] += 1
+
+
+def isValid(val, board, row, col):
+	isOk = True
+	for i in range(0,9):
+		if(i != col and board[row][i] == val):
+			isOk = False
+
+	for i in range(0,9):
+		if(i != row and board[i][col] == val):
+			isOk = False
+
+	for i in range(0,3):
+		i += int(row/3)*3
+		for j in range(0,3):
+			j += int(col/3)*3
+			if(i != row and j != col and board[i][j] == val):
+				isOk = False
+
+	return isOk
+
+
+def fillValue(board, row, col):
+	found = False
+	for i in range(1,10):
+		if(isValid(i, board, row, col) and i > board[row][col]):
+			board[row][col] = i
+			found =  True
+			break
+	return found
+
+
+def gotoNext(row, col):
+	if(col==8):
+		col = 0
+		row += 1
+	else:
+		col+=1
+
+	return row, col
+
+
+def getPrevious(filled, row, col):
+	while True:
+		if(col==0):
+			col = 8
+			row -= 1
+		else:
+			col -= 1
+
+		if(filled[row][col]):
+			return row, col
+
+
+def solve(problem, filled):
+	createSuppliment(problem, filled)
+	row, col = 0, 0
+	while(row < 9 and col < 9):
+		if(filled[row][col]):
+			isFilled = fillValue(problem, row, col)
+		else:
+			isFilled = True
+
+		if(not isFilled):
+			problem[row][col] = 0
+			row, col = getPrevious(filled, row, col)
+		else:
+			row, col = gotoNext(row, col)
+		clear()
+		showBoard(problem)
+		time.sleep(0.01)
+
+
+def clear(): 
+    if os.name == 'nt': # for windows 'nt' and for mac+linux 'posix'
+        _ = os.system('cls') 
+    else: 
+        _ = os.system('clear')
+
+
+def main():
+	print("Sudoku-Solver")
+	print("1. Enter problem")
+	print("2. Sample problem")
+	choice = int(input(">> "))
+	# solve(problem, filled)
+	# showBoard(filled)
+	# print(getPrevious(filled,0,7))
+
+
+if __name__ == '__main__':
+	main()
